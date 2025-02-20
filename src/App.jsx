50f88d48d52e8c65bebb87e022b9a1efb702e83a
ireshanananda <73053441+ireshanananda@users.noreply.github.com>
2025-02-21 00:34:00 +0530

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
 
   return (
-    <div>
+    <div className="app-container">
       <Navbar cartItems={cartItems} />
       <div className="container">
         <Outlet context={[cartItems, setCartItems]} />
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        &copy; {new Date().getFullYear()} GreenCart LK. All rights reserved.
+      </p>
+      <p>
+        <a href="/about">About Us</a> | <a href="/contact">Contact</a> |{" "}
+        <a href="/privacy">Privacy Policy</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
